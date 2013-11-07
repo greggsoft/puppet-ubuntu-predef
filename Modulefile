25c7b01ded386c20a1924753851153c8990b3071
Mikhail Menshenin <greggsoft@gmail.com>
2013-11-07 10:30:35 +0600

    ini

---
+++ Modulefile
@@ -0,0 +1,16 @@
+name    'greggsoft-ubuntu-predef'
+version '0.1.0'
+source 'UNKNOWN'
+author 'greggsoft'
+license 'Apache License, Version 2.0'
+summary 'UNKNOWN'
+description 'UNKNOWN'
+project_page 'UNKNOWN'
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
+dependency 'attachmentgenie/locales', '>= 0.0.1'
+dependency 'attachmentgenie/timezone', '>= 0.0.1'
+dependency 'puppetlabs/ntp', '>= 0.0.1'
+# Not yet in puppet forge
+# dependency 'greggsoft/puppet', '>= 0.0.1' 
---
+++ manifests/init.pp
@@ -0,0 +1,50 @@
+# == Class: ubuntu-predef
+#
+# Full description of class ubuntu-predef here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { ubuntu-predef:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2013 Your name here, unless otherwise noted.
+#
+class ubuntu-predef {
+	include ::puppet
+	include ::ntp
+
+	class { 'locales':
+		default_value => 'en_US.UTF-8',
+		available     => ['ru_RU.UTF-8 UTF-8'],
+	}
+
+	class {'timezone':
+		zone => 'Asia/Yekaterinburg'
+	}
+}
